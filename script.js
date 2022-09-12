900a28612cdf7cc5f18543279c6eebbd9bd410dd
unknown <ansari.shariq.badshah@gmail.com>
2022-09-12 19:10:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels=document.querySelectorAll('.panel')
+panels.forEach((panel)=>{
+    console.log(panel)
+  panel.addEventListener("mouseenter",()=>{
+    removeActiveClasses()
+    panel.classList.add("active");
+  })//
+})
+function removeActiveClasses(){
+    panels.forEach((panel)=>{
+        panel.classList.remove("active")
+    })
+}
